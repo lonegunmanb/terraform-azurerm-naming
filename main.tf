67d8476bd957b578d953714ee585ac43c6502c8d
zjhe <hezijie@microsoft.com>
2022-05-07 14:08:45 +0800

    should be stopped by gencheck

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ locals {
   # adding a first letter to guarantee that you always start with a letter
   random_safe_generation = join("", [random_string.first_letter.result, random_string.main.result])
   random                 = substr(coalesce(var.unique-seed, local.random_safe_generation), 0, var.unique-length)
-  prefix                 = join("-", var.prefix)
+  prefix                 = join("_", var.prefix)
   prefix_safe            = lower(join("", var.prefix))
   suffix                 = join("-", var.suffix)
   suffix_unique          = join("-", concat(var.suffix, [local.random]))
