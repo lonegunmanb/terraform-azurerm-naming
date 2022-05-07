7114fa7c1884b4276392f88bd74a1214d417b641
zjhe <hezijie@microsoft.com>
2022-05-07 14:19:53 +0800

    should be stopped by tfvalidate

--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@ variable "prefix" {
   description = "It is not recommended that you use prefix by azure you should be using a suffix for your resources."
 }
 
-variable "suffix" {
+variable "suffix"
   type        = list(string)
   default     = []
   description = "It is recommended that you specify a suffix for consistency. please use only lowercase characters when possible"
